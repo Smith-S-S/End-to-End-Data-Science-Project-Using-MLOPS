a6dcbd93a4c1fa388996268f4064495022e2e053
Smith-S-S <ss9130@srmist.edu.in>
2024-05-27 20:10:36 +0530

    data ingesion over

---
+++ main.py
@@ -0,0 +1,14 @@
+from mlproject import logger
+from mlproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
